fbdda23c818cdd7ec5578295eb77bc409598c758
Ben Haswell <haswelba@miamioh.edu>
2023-10-25 11:09:31 -0400

    Began search bar

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,36 @@ function App() {
 
   return (
     <>
-    <NavBar/>
+      <NavBar />
 
-    {/* Background */}
-    <div className="background">
-      <h1 className='font-extrabold text-yellow text-6xl'>Glue</h1>
-      <h2 className='font-extrabold text-white text-4xl'>Your Digital App Oasis</h2>
-    </div>
+      {/* Background */}
+      <div className="background">
+        <h1 className='font-extrabold text-yellow text-6xl'>Glue</h1>
+        <h2 className='font-extrabold text-white text-4xl'>Your Digital App Oasis</h2>
+      </div>
 
-    <main>
-      {/* Search Bar */}
-      <h2 className='font-extrabold text-3xl m-12'>search bar will go here</h2>
+      <main>
 
-      {/* Apps Display */}
-      <h2 className='font-extrabold text-3xl m-12'>All Apps</h2>
-    </main>
-    
+        {/* Search Bar */}
+        {/* <div className='flex items-center justify-center'>
+          <form className="w-full max-w-xl">
+            <div className="border-teal py-2">
+              <input className="w-full" type="text" placeholder="Search Apps" aria-label="Search Apps" />
+            </div>
+          </form>
+        </div> */}
+        <div className='flex items-center justify-center'>
+          <form className="w-full max-w-xl">
+            <input className="w-full" type="text" placeholder="Search Apps" aria-label="Search Apps" />
+          </form>
+        </div>
+        
+
+
+        {/* Apps Display */}
+        <h2 className='font-extrabold text-3xl m-12'>All Apps</h2>
+      </main>
+
     </>
   )
 }
