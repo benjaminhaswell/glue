0f6990e94e744b72d35134dc8f306a01f91e0cbc
Ben Haswell <benjaminallardhaswell@gmail.com>
2023-11-20 18:46:09 -0500

    Filters displayed - need to implement functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import AppCard from './components/AppCard';
-import './components/NavBar.jsx';
+import Filters from './components/Filters.jsx';
 
 function App() {
-  
   const [allApps, setAllApps] = useState([]);
+  const [filters, setFilters] = useState({
+    payment: 'all',
+    platform: 'all',
+  });
 
-
-  // Fetch apps from server
   useEffect(() => {
     async function fetchApps() {
       try {
         const response = await fetch('http://localhost:3000/api/apps');
-        
+
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
@@ -26,38 +27,41 @@ function App() {
       }
     }
 
-    // Call the function to fetch data
     fetchApps();
-  }, []); // Empty dependency array to ensure it only runs once
+  }, []);
 
-  // Render loading or the actual component based on data
+  const handleFilterChange = (filterType, value) => {
+    setFilters((prevFilters) => ({ ...prevFilters, [filterType]: value }));
+    // Filtering logic goes here
+    console.log(`Filter type: ${filterType}, Value: ${value}`);
+  };
+
   return (
     <>
-      {/* Background */}
       <div className="background">
         <h1 className='font-extrabold text-yellow text-6xl'>Glue</h1>
         <h2 className='font-extrabold text-white text-4xl'>Your Digital App Oasis</h2>
       </div>
 
       <main>
-        {/* Check if data is available */}
         {allApps.length === 0 ? (
           <p>Loading...</p>
         ) : (
           <>
-            {/* Search Bar */}
+            {/* Search Apps */}
             <div className='flex items-center justify-center'>
               <form className='mt-8'>
                 <input type="text" id="searchApps" placeholder="Search Apps" aria-label="Search Apps" />
               </form>
             </div>
 
-            {/* Filters go here */}
+            {/* Filters */}
+            <div className="flex justify-center mb-8">
+              <Filters filters={filters} handleFilterChange={handleFilterChange} />
+            </div>
 
-
-            {/* Apps Display */}
+            {/* Apps */}
             <h2 className='font-extrabold text-3xl m-12'>All Apps</h2>
-            {/* This element VVV is the culprit of the width issue */}
             <div className="grid grid-cols-3 gap-4 px-8 lg:px-48">
               {allApps.map((appObject, index) => (
                 <AppCard key={index} appObject={appObject} />
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,57 +1,41 @@
 import PropTypes from 'prop-types';
 
 const Filters = ({ filters, handleFilterChange }) => {
-    return (
-        <div className="bg-gray-200 p-4 rounded-md">
-            <h2 className="text-lg font-semibold mb-2">Filters</h2>
-            <label className="flex items-center mb-2">
-                <input
-                    type="checkbox"
-                    checked={filters.free}
-                    onChange={() => handleFilterChange('free')}
-                    className="mr-2"
-                />
-                Free
-            </label>
-            <label className="flex items-center mb-2">
-                <input
-                    type="checkbox"
-                    checked={filters.paid}
-                    onChange={() => handleFilterChange('paid')}
-                    className="mr-2"
-                />
-                Paid
-            </label>
-            <label className="flex items-center mb-2">
-                <input
-                    type="checkbox"
-                    checked={filters.iOS}
-                    onChange={() => handleFilterChange('iOS')}
-                    className="mr-2"
-                />
-                iOS
-            </label>
-            <label className="flex items-center">
-                <input
-                    type="checkbox"
-                    checked={filters.android}
-                    onChange={() => handleFilterChange('android')}
-                    className="mr-2"
-                />
-                Android
-            </label>
-        </div>
-    );
+  return (
+    <div className="bg-gray-200 p-4 rounded-md flex items-center">
+      <h2 className="text-lg font-semibold mr-4">Price:</h2>
+
+      {/* Dropdown for Free, Paid, All */}
+      <select
+        value={filters.payment}
+        onChange={(e) => handleFilterChange('payment', e.target.value)}
+        className="mr-4"
+      >
+        <option value="all">All</option>
+        <option value="free">Free</option>
+        <option value="paid">Paid</option>
+      </select>
+
+      {/* Dropdown for iOS, Android, All */}
+      <h2 className="text-lg font-semibold mr-4">Platform:</h2>
+      <select
+        value={filters.platform}
+        onChange={(e) => handleFilterChange('platform', e.target.value)}
+      >
+        <option value="all">All</option>
+        <option value="iOS">iOS</option>
+        <option value="android">Android</option>
+      </select>
+    </div>
+  );
 };
 
 Filters.propTypes = {
-    filters: PropTypes.shape({
-        free: PropTypes.bool.isRequired,
-        paid: PropTypes.bool.isRequired,
-        iOS: PropTypes.bool.isRequired,
-        android: PropTypes.bool.isRequired,
-    }).isRequired,
-    handleFilterChange: PropTypes.func.isRequired,
+  filters: PropTypes.shape({
+    payment: PropTypes.oneOf(['all', 'free', 'paid']).isRequired,
+    platform: PropTypes.oneOf(['all', 'iOS', 'android']).isRequired,
+  }).isRequired,
+  handleFilterChange: PropTypes.func.isRequired,
 };
 
 export default Filters;
